5268d271b4e90585c329e36cc65c94e6d27042f7
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-08-27 19:00:11 +0300

    Add task 8 ascii observation solution

--- Solution_8/assets/js/scripts.js
+++ Solution_8/assets/js/scripts.js
@@ -34,3 +34,22 @@ let sum_handle = () =>{
 }
 if (document.getElementById('sum_button')) sum_button.addEventListener("click", sum_handle);
 //#endregion
+
+//#region ASCII
+let ascii_observe_toggle = false;
+let ascii_observe_toggle_handle = (event) =>{
+    const toggle_classList = event.currentTarget.classList;
+    ascii_observe_toggle = !ascii_observe_toggle;
+    toggle_classList.toggle("active");
+}
+let keyboard_key_pressed = (event) => {
+    if (ascii_observe_toggle) {
+        if (event.altKey) alert("ALT key was pressed");
+        else if (event.ctrlKey) alert("CTRL key was pressed");
+        else if (event.shiftKey) alert("SHIFT key was pressed");
+        else alert(`Key pressed was of code ${event.keyCode}`);
+    }
+}
+if (document.getElementById('ascii_button')) ascii_button.addEventListener("click", ascii_observe_toggle_handle);
+document.addEventListener("keydown", keyboard_key_pressed)
+//#endregion
