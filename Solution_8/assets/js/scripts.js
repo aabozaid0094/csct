60f9aa0b833f2f442de97db8c8aec41fd3638710
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-08-27 18:37:18 +0300

    Add task 8 parameters count solution

--- Solution_8/assets/js/scripts.js
+++ Solution_8/assets/js/scripts.js
@@ -1,2 +1,18 @@
 'use strict';
 
+//#region Parameters
+let generate_parameters = (parameters_count) => {
+    return Array.from(Array(parameters_count).keys())
+}
+function parameters_exception(...parameters) {
+    if(arguments.length!=2) throw new Error("Parameters count is not correct")
+    else console.log("Parameters count is correct");
+}
+let parameters_handle = () => {
+    let parameters_count_message = prompt("Enter a non-negative integer number of parameters");
+    if (!isNaN(parameters_count_message) && Number.isInteger(Number(parameters_count_message)) && Number(parameters_count_message) > 0) 
+    parameters_exception(...generate_parameters(Number(parameters_count_message)));
+    else alert('Wrong number!!');
+}
+if (document.getElementById('parameters_button')) parameters_button.addEventListener("click", parameters_handle);
+//#endregion
