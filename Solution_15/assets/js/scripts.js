d86eba1b350a6a9b6d10b3f7ba6054c05d6a6ebd
Ahmed Abd El-Aziz Ezzat Mohamed <aaezzat@mcit.gov.eg>
2023-08-30 12:41:21 +0300

    Add starter files of task 15

---
+++ Solution_15/assets/js/scripts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//#region Object
+let object_handle = () => {
+
+};
+if(document.getElementById('object')) object.addEventListener("click", object_handle);
+//#endregion
+
+//#region Iterator
+let iterator_handle = () => {
+
+};
+if(document.getElementById('iterator')) iterator.addEventListener("click", object_handle);
+//#endregion
+
+document.addEventListener("DOMContentLoaded", function(event){
+});
