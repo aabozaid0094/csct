72c85dfc5cd82ad3778e1b3393cf40cbc897d4b8
Ahmed Abd El-Aziz Ezzat Mohamed <aaezzat@mcit.gov.eg>
2023-08-31 14:43:18 +0300

    Add fibonacci solution without generators

--- Solution_16/assets/js/scripts.js
+++ Solution_16/assets/js/scripts.js
@@ -24,5 +24,30 @@ let promise_handle = () => {
 if (document.getElementById('promise_button')) promise_button.addEventListener("click", promise_handle);
 //#endregion
 
+//#region Fibonacci
+let fibonacci_length = (length) => {
+    if (length < 2) throw new Error("Not enough fibonacci length");
+    let result = [0, 1];
+    for (let index = 2; index < length; index++)
+        result.push(result[index - 1] + result[index - 2]);
+    return result;
+};
+let fibonacci_max = (max) => {
+    if (max < 1) throw new Error("Not enough fibonacci max");
+    let result = [0, 1], next_fibonacci = 1;
+    for (let index = 2; next_fibonacci < max; index++) {
+        console.log(next_fibonacci);
+        result.push(next_fibonacci);
+        next_fibonacci = result[index] + result[index - 1];
+    }
+    return result;
+};
+let fibonacci_handle = () => {
+    console.log(fibonacci_length(10));
+    console.log(fibonacci_max(10));
+};
+if (document.getElementById('fibonacci_button')) fibonacci_button.addEventListener("click", fibonacci_handle);
+//#endregion
+
 document.addEventListener("DOMContentLoaded", function (event) {
 });
