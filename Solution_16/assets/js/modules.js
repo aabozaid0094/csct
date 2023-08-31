ba4ce8141da72c3345f302da6f271bf95506be1c
Ahmed Abd El-Aziz Ezzat Mohamed <aaezzat@mcit.gov.eg>
2023-08-31 14:09:14 +0300

    Add separate script file that handles all modules

---
+++ Solution_16/assets/js/modules.js
@@ -0,0 +1,15 @@
+import Rectangle from "./modules/Rectangle.js";
+import Square from "./modules/Square.js";
+import Circle from "./modules/Circle.js";
+import Triangle from "./modules/Triangle.js";
+'use strict';
+
+//#region Areas
+let areas_handle = () => {
+    console.log("Rectangle(3,4)", new Rectangle(3,4).toString());
+    console.log("Square(3)", new Square(3).toString());
+    console.log("Circle(3)", new Circle(3).toString());
+    console.log("Triangle(3,4)", new Triangle(3,4).toString());
+};
+if(document.getElementById('areas')) areas.addEventListener("click", areas_handle);
+//#endregion
