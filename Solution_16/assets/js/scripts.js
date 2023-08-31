43d7749bbb067c1472c00feb8d80ffdd9a52d277
Ahmed Abd El-Aziz Ezzat Mohamed <aaezzat@mcit.gov.eg>
2023-08-31 08:44:20 +0300

    Add starter files of task 16

---
+++ Solution_16/assets/js/scripts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//#region Button
+let button_handle = () => {
+    console.log("button_handle");
+};
+if(document.getElementById('button')) button.addEventListener("click", button_handle);
+//#endregion
+
+document.addEventListener("DOMContentLoaded", function(event){
+});
