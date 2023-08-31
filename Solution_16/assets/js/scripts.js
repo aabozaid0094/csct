fe14c3ec7a138d57471aee2349bc3322723b79c7
Ahmed Abd El-Aziz Ezzat Mohamed <aaezzat@mcit.gov.eg>
2023-08-31 14:11:47 +0300

    Add promise task solution

--- Solution_16/assets/js/scripts.js
+++ Solution_16/assets/js/scripts.js
@@ -1,11 +1,28 @@
 'use strict';
 
-//#region Button
-let button_handle = () => {
-    console.log("button_handle");
+//#region Promise
+const is_valid_url = urlString => {
+    try {
+        return Boolean(new URL(urlString));
+    }
+    catch (e) {
+        return false;
+    }
+}
+let url_to_promise = (url) => {
+    if (is_valid_url(url)) {
+        return fetch(url);
+    }
+    else return "Invalid endpoint!";
 };
-if(document.getElementById('button')) button.addEventListener("click", button_handle);
+let promise_handle = () => {
+    url_to_promise("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(error => console.log(error));
+};
+if (document.getElementById('promise')) promise.addEventListener("click", promise_handle);
 //#endregion
 
-document.addEventListener("DOMContentLoaded", function(event){
+document.addEventListener("DOMContentLoaded", function (event) {
 });
